f0fc3f1f422d65d51b488b61ca06ca9e9e272146
AVR <avrivadeneira1@gmail.com>
2020-06-10 19:30:09 -0400

    Add client folder back to git repo

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const path = require("path");
 const { nanoid } = require("nanoid");
 require("dotenv").config();
 
-console.log("ASDSA", process.env.MONGO_URI);
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
